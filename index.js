a82fc0c3ac3a8403f26921f0caf01aa302857872
Anshul5678 <anshulmeshram22@gmail.com>
2024-11-28 10:23:09 +0530

    xyz

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function convert() {
  temp = Number(textBox.value) ;
   temp = temp * 9/5 + 32;
    result.textContent = temp.toFixed(1) + "°F";
-      
     } else if(toCelsius.checked) {
         temp = Number(textBox.value);
         temp = (temp - 32) * 5/9;
